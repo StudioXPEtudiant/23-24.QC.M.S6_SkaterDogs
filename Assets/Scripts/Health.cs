07a3bc862bc5ab1fbd440f10ceb72cbe06c173cd
Nathan <beaudetnatha@gmail.com>
2025-01-22 20:06:31 -0500

    Le chat attaque

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,13 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField] private GameObject lava;
+    [SerializeField] private GameObject red;
+    [SerializeField] private GameObject yellow;
+    [SerializeField] private GameObject green;
+    [SerializeField] private GameObject white;
+    [SerializeField] private Slider slider;
     [SerializeField]private bool detectCat;
     [SerializeField]private GameObject me;
-   [SerializeField] private int health;
+   [SerializeField] public int health;
    [SerializeField] private int numberToDecrease;
 
     private void Start()
@@ -15,21 +23,52 @@ public class Health : MonoBehaviour
         health = 100;
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Cattacker")) ;
-        {
-            //Decreased();
-        }
-       
-    }
-
+   
+   
     private void Update()
     {
+        
+        slider.value = health;
         if (health < 0)
         {
-            me.SetActive(false);
+            me.transform.position = new Vector3(0, 4, 0);
+                health=100;
         }
+        
+        if (slider.value < 16)
+        {
+            red.SetActive(true);
+        }
+        else
+        {
+            red.SetActive(false);
+        }
+        
+        if (slider.value < 51 && slider.value > 15)
+        {
+            yellow.SetActive(true);
+        }
+        else
+        {
+            yellow.SetActive(false);
+        }
+        if (slider.value < 100 && slider.value > 50)
+        {
+            green.SetActive(true);
+        }
+        else
+        {
+            green.SetActive(false);
+        }
+        if (slider.value == 100)
+        {
+            white.SetActive(true);
+        }
+        else
+        {
+            white.SetActive(false);
+        }
+
     }
 
     [ContextMenu("Decrease")]
